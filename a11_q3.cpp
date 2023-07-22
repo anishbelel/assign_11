912e6c65c0bd923693edfb0a80393efc5ab60a33
anishbelel <134290462+anishbelel@users.noreply.github.com>
2023-07-22 18:41:03 +0530

    Add files via upload

---
+++ a11_q3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int merge(vector<vector<int>>& s) {
+vector<vector<int>> ans;
+int j=0;
+ans.push_back(s[0]);
+int count=0;
+for(int i=1;i<s.size();i++)
+{
+if(ans[j][1]>s[i][0])
+{
+count++;
+ans[j][1]=max(ans[j][1],s[i][1]);
+}else{
+j++;
+ans.push_back(s[i]);
+}
+}
+return count;
+}
+int main()
+{
+vector<vector<int>> mat={{1,2},{2,3},{3,4},{4,5}};
+cout<<merge(mat);
+}
